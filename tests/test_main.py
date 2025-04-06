c51ac5b90b740324e19bd26704571c79ee7a1f59
Damien Burks <damien@damienjburks.com>
2025-04-06 00:44:37 -0500

    removing tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,16 +5,3 @@ This module uses the FastAPI TestClient to perform unit tests on endpoints
 defined in the `main` FastAPI app. The tests include checking responses for
 various endpoints under normal and edge-case scenarios.
 """
-
-# pylint: disable=wrong-import-position
-
-import sys
-import os
-from fastapi.testclient import TestClient
-
-# Add the parent directory to the system path for imports
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from main import app  # Adjust the import if your file is named differently
-
-client = TestClient(app)
